751ecf82853495213ad7364b9794468f0f40d6ae
Emil Nymann Sølyst <emilnymann96@gmail.com>
2026-05-31 08:53:24 +0200

    fix(waybar): make waybar enable option instead of yazi

--- modules/features/desktop/waybar.nix
+++ modules/features/desktop/waybar.nix
@@ -1,9 +1,9 @@
 { self, inputs, ... }:
 {
 	flake.nixosModules.waybar = { pkgs, lib, ... }: {
-		options.nixosModules.yazi.enable = lib.mkEnableOption "waybar";
+		options.nixosModules.waybar.enable = lib.mkEnableOption "waybar";
 		config = {
-			nixosModules.yazi.enable = true;
+			nixosModules.waybar.enable = true;
 			fonts.packages = with pkgs; [ font-awesome_4 ];
 			programs.waybar.enable = true;
 		};
