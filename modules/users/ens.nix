1077f717a5bbadb80267ea8b92e6d88a510e5e05
Emil Nymann Sølyst <emilnymann96@gmail.com>
2026-06-01 15:18:53 +0200

    chore: add nix editor deps to user ens

--- modules/users/ens.nix
+++ modules/users/ens.nix
@@ -1,31 +1,46 @@
-{ self, inputs, lib, ... }:
+{ self, ... }:
 {
-	flake.userModules.ens = { pkgs, lib, config, ... }: {
-		hjem.users.ens = {
-			imports = with self.homeModules; [
-				gtk-conf
-				ghostty
-				fish
-				darkmode
-				cursor-theme.rose-pine
-        lazyvim
-			];
+  flake.userModules.ens =
+    {
+      pkgs,
+      lib,
+      config,
+      ...
+    }:
+    {
+      hjem.users.ens = {
+        imports = with self.homeModules; [
+          gtk-conf
+          ghostty
+          fish
+          darkmode
+          cursor-theme.rose-pine
+          lazyvim
+        ];
 
-			xdg.config.files = {
-				"hypr/hyprland.lua".source = ./config/hypr/hyprland.lua;
-				"hypr/hyprland".source = ./config/hypr/hyprland;
+        config = {
+          programs.lazyvim.extraPackages = with pkgs; [
+            cargo
+            statix
+            nixfmt
+          ];
 
-				"git/config".text = ''
-					[user]
-						name = Emil Nymann Sølyst
-						email = emilnymann96@gmail.com
-						signingkey = 45E51048D62204CCD70B633B31D710749D7D8E7B
-					[commit]
-						gpgsign = true
-					[tag]
-						gpgsign = true
-				'';
-			};
-		};
-	};
+          xdg.config.files = {
+            "hypr/hyprland.lua".source = ./config/hypr/hyprland.lua;
+            "hypr/hyprland".source = ./config/hypr/hyprland;
+
+            "git/config".text = ''
+              [user]
+                name = Emil Nymann Sølyst
+                email = emilnymann96@gmail.com
+                signingkey = 45E51048D62204CCD70B633B31D710749D7D8E7B
+              [commit]
+                gpgsign = true
+              [tag]
+                gpgsign = true
+            '';
+          };
+        };
+      };
+    };
 }
