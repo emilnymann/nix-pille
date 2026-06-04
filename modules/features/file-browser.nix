fad1f58c0eea7bd6f549f868ca643e03c31db765
Emil Nymann Sølyst <emilnymann96@gmail.com>
2026-06-04 08:48:46 +0200

    feat(file-browser): add yazi ripdrag plugin

--- modules/features/file-browser.nix
+++ modules/features/file-browser.nix
@@ -1,5 +1,25 @@
 _: {
-  flake.homeModules.file-browser = _: {
-    programs.yazi.enable = true;
-  };
+  flake.homeModules.file-browser =
+    { pkgs, ... }:
+    {
+      programs.yazi = {
+        enable = true;
+
+        extraPackages = with pkgs; [
+          ripdrag
+        ];
+
+        plugins = {
+          drag = pkgs.yaziPlugins.drag;
+        };
+
+        keymap.mgr.prepend_keymap = [
+          {
+            on = [ "<C-d>" ];
+            run = "plugin drag";
+            desc = "Drag Files";
+          }
+        ];
+      };
+    };
 }
