b50fe00affbf218ef8288a56d5562b988278ae80
Emil Nymann Sølyst <emilnymann96@gmail.com>
2026-06-08 09:18:27 +0200

    feat(nixvim): add dashboard

---
+++ modules/features/neovim-ide/nixvim/plugins/_snacks-dashboard.nix
@@ -0,0 +1,72 @@
+_: {
+  programs.nixvim = {
+    plugins.snacks = {
+      enable = true;
+      settings = {
+        dashboard = {
+          enabled = true;
+          preset = {
+            header = ''
+              @@@  @@@  @@@  @@@  @@@  @@@  @@@  @@@  @@@@@@@@@@ 
+              @@@@ @@@  @@@  @@@  @@@  @@@  @@@  @@@  @@@@@@@@@@@
+              @@!@!@@@  @@!  @@!  !@@  @@!  @@@  @@!  @@! @@! @@!
+              !@!!@!@!  !@!  !@!  @!!  !@!  @!@  !@!  !@! !@! !@!
+              @!@ !!@!  !!@   !@@!@!   @!@  !@!  !!@  @!! !!@ @!@
+              !@!  !!!  !!!    @!!!    !@!  !!!  !!!  !@!   ! !@!
+              !!:  !!!  !!:   !: :!!   :!:  !!:  !!:  !!:     !!:
+              :!:  !:!  :!:  :!:  !:!   ::!!:!   :!:  :!:     :!:
+              ::   ::   ::   ::  :::    ::::     ::  :::     ::  
+              ::    :   :     :   ::      :      :     :      :  
+            '';
+            keys = [
+              {
+                icon = " ";
+                key = "f";
+                desc = "Find File";
+                action = ":lua Snacks.dashboard.pick('files')";
+              }
+              {
+                icon = " ";
+                key = "n";
+                desc = "New File";
+                action = ":ene | startinsert";
+              }
+              {
+                icon = " ";
+                key = "g";
+                desc = "Find Text";
+                action = ":lua Snacks.dashboard.pick('live_grep')";
+              }
+              {
+                icon = " ";
+                key = "r";
+                desc = "Recent Files";
+                action = ":lua Snacks.dashboard.pick('old_files')";
+              }
+              {
+                icon = " ";
+                key = "c";
+                desc = "Config";
+                action = ":lua Snacks.dashboard.pick('files', { cwd = '/etc/nixos/modules/features/neovim-ide'})";
+              }
+              {
+                icon = " ";
+                key = "q";
+                desc = "Quit";
+                action = ":qa";
+              }
+            ];
+          };
+          sections = [
+            { section = "header"; }
+            {
+              section = "keys";
+              gap = 1;
+              padding = 1;
+            }
+          ];
+        };
+      };
+    };
+  };
+}
