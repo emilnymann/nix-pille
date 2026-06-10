08092a18695393b5e053d60a915037813f68ce65
Emil Nymann Sølyst <emilnymann96@gmail.com>
2026-06-10 06:53:46 +0200

    refactor(desktop): move ashell config out of main desktop module

--- modules/features/desktop/desktop.nix
+++ modules/features/desktop/desktop.nix
@@ -39,6 +39,7 @@ _: {
         ./hyprland/_config.nix
         ./hyprland/_monitors.nix
         ./hyprland/_binds.nix
+        ./_ashell.nix
       ];
 
       home.packages = with pkgs; [
@@ -76,77 +77,6 @@ _: {
           env = "TERMCMD='ghostty -e'";
           open_mode = "suggested";
           save_mode = "suggested";
-        };
-      };
-
-      programs.ashell = {
-        enable = true;
-        systemd.enable = true;
-        settings = {
-          modules = {
-            left = [
-              "Workspaces"
-              "WindowTitle"
-            ];
-            center = [
-              "Tempo"
-              "MediaPlayer"
-            ];
-            right = [
-              "Tray"
-              "Notifications"
-              "SystemInfo"
-              "Settings"
-            ];
-          };
-
-          media_player = {
-            indicator_format = "IconAndTitle";
-          };
-
-          notifications = {
-            grouped = true;
-          };
-
-          settings = {
-            peripheral_indicators = "All";
-            indicators = [
-              "Audio"
-              "Microphone"
-              "Network"
-              "Bluetooth"
-              "PeripheralBattery"
-            ];
-            bluetooth_more_cmd = "${lib.getExe pkgs.ghostty} -e ${lib.getExe pkgs.bluetui}";
-          };
-
-          tempo = {
-            clock_format = "%e %R ";
-          };
-
-          tray = {
-            right_click = "menu";
-          };
-
-          workspaces = {
-            disable_special_workspaces = true;
-            enable_workspace_filling = true;
-            max_workspaces = 9;
-            workspace_names = [
-              "一"
-              "二"
-              "三"
-              "四"
-              "五"
-              "六"
-              "七"
-              "八"
-              "九"
-              "十"
-            ];
-          };
-
-          osd.enabled = true;
         };
       };
 
