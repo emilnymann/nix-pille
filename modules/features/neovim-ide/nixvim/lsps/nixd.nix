5199904ba404f987a8474f14ac92e07cc18b2bca
Emil Nymann Sølyst <emilnymann96@gmail.com>
2026-06-11 18:04:56 +0200

    feat(nixvim): use alejandra as formatter for nixd

--- modules/features/neovim-ide/nixvim/lsps/nixd.nix
+++ modules/features/neovim-ide/nixvim/lsps/nixd.nix
@@ -1,23 +1,25 @@
 _: {
-  flake.homeModules.neovim-ide =
-    { pkgs, ... }:
-    {
-      programs.nixvim.lsp = {
-        servers = {
-          nixd = {
-            enable = true;
-            package = pkgs.nixd;
-            config = {
-              cmd = [ "nixd" ];
-              filetypes = [ "nix" ];
-              root_markers = [
-                "flake.nix"
-                ".git"
-              ];
-              settings.nixd.formatting.command = [ "${pkgs.nixfmt}/bin/nixfmt" ];
-            };
+  flake.homeModules.neovim-ide = {
+    pkgs,
+    lib,
+    ...
+  }: {
+    programs.nixvim.lsp = {
+      servers = {
+        nixd = {
+          enable = true;
+          package = pkgs.nixd;
+          config = {
+            cmd = ["nixd"];
+            filetypes = ["nix"];
+            root_markers = [
+              "flake.nix"
+              ".git"
+            ];
+            settings.nixd.formatting.command = [(lib.getExe pkgs.alejandra)];
           };
         };
       };
     };
+  };
 }
