e87eeadf77a0644f22089d31cf9985e0dc4318c6
Emil Nymann Sølyst <ens@uniify.io>
2026-07-06 13:04:18 +0200

    feat(neovim-ide): use snacks picker for lsp symbol lists

--- modules/features/neovim-ide/nixvim/keymaps/lsp.nix
+++ modules/features/neovim-ide/nixvim/keymaps/lsp.nix
@@ -4,31 +4,34 @@ _: {
       {
         mode = ["n"];
         key = "gd";
-        action.__raw = "function() vim.lsp.buf.definition() end";
+        action.__raw = "function() Snacks.picker.lsp_definitions() end";
         options = {desc = "Goto definition";};
       }
       {
         mode = ["n"];
         key = "gr";
-        action.__raw = "function() vim.lsp.buf.references() end";
-        options = {desc = "Goto references";};
+        action.__raw = "function() Snacks.picker.lsp_references() end";
+        options = {
+          desc = "Goto references";
+          nowait = true;
+        };
       }
       {
         mode = ["n"];
         key = "gI";
-        action.__raw = "function() vim.lsp.buf.implementation() end";
+        action.__raw = "function() Snacks.picker.lsp_implementations() end";
         options = {desc = "Goto Implementation";};
       }
       {
         mode = ["n"];
         key = "gy";
-        action.__raw = "function() vim.lsp.buf.type_definition() end";
+        action.__raw = "function() Snacks.picker.lsp_type_definitions() end";
         options = {desc = "Goto t[y]pe definition";};
       }
       {
         mode = ["n"];
         key = "gD";
-        action.__raw = "function() vim.lsp.buf.declaration() end";
+        action.__raw = "function() Snacks.picker.lsp_declarations() end";
         options = {desc = "Goto Declaration";};
       }
       {
