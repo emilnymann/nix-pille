39cd2d0b7099cd1b5fd830ffdc30de5ffb3fc614
Emil Nymann Sølyst <ens@uniify.io>
2026-07-09 07:40:32 +0200

    fix(neovim-ide): nixd flake options evaluate without a hardcoded root dir

--- .nvim/lsp/nixd.lua
+++ .nvim/lsp/nixd.lua
@@ -1,20 +1,67 @@
+local root = vim.fs.root(0, { "flake.nix", ".git" }) or vim.uv.cwd()
+
+local function from_flake(expr)
+  return string.format(
+    [[
+let
+  flake = builtins.getFlake %q;
+in
+%s
+]],
+    root,
+    expr
+  )
+end
+
+local function merge_config_options(attr)
+  return from_flake(string.format(
+    [[
+let
+  lib = flake.inputs.nixpkgs.lib;
+in
+lib.foldl' lib.recursiveUpdate { }
+  (map (cfg: cfg.options) (builtins.attrValues flake.%s))
+]],
+    attr
+  ))
+end
+
+local function merge_home_manager_options()
+  return from_flake([[
+let
+  lib = flake.inputs.nixpkgs.lib;
+  merge = configs:
+    lib.foldl' lib.recursiveUpdate { }
+      (map
+        (cfg: cfg.options.home-manager.users.type.getSubOptions [])
+        (builtins.attrValues configs));
+in
+lib.recursiveUpdate
+  (merge flake.nixosConfigurations)
+  (merge flake.darwinConfigurations)
+]])
+end
+
 vim.lsp.config("nixd", {
-	settings = {
-		nixd = {
-			nixpkgs = {
-				expr = [[import (builtins.getFlake "/etc/nixos").inputs.nixpkgs { }]],
-			},
-			options = {
-				nixos = {
-					expr = [[(builtins.getFlake "/etc/nixos").nixosConfigurations.argon.options]],
-				},
-				["home-manager"] = {
-					expr = [[(builtins.getFlake "/etc/nixos").nixosConfigurations.argon.options.home-manager.users.type.getSubOptions []]],
-				},
-        ["flake-parts"] = {
-          expr = [[(builtins.getFlake "/etc/nixos").debug.options]],
+  settings = {
+    nixd = {
+      nixpkgs = {
+        expr = from_flake("import flake.inputs.nixpkgs { }"),
+      },
+      options = {
+        nixos = {
+          expr = merge_config_options("nixosConfigurations"),
         },
-			},
-		},
-	},
+        ["nix-darwin"] = {
+          expr = merge_config_options("darwinConfigurations"),
+        },
+        ["home-manager"] = {
+          expr = merge_home_manager_options(),
+        },
+        ["flake-parts"] = {
+          expr = from_flake("flake.debug.options"),
+        },
+      },
+    },
+  },
 })
