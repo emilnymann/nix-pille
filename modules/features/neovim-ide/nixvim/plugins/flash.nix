0d3cddbeb9c643c54205ecc759825c2a80979afb
Emil Nymann Sølyst <ens@uniify.io>
2026-07-10 16:54:19 +0200

    feat(neovim-ide/flash): add plugin and keymaps

---
+++ modules/features/neovim-ide/nixvim/plugins/flash.nix
@@ -0,0 +1,36 @@
+_: {
+  flake.homeModules.neovim-ide = _: {
+    programs.nixvim = {
+      plugins.flash = {
+        enable = true;
+      };
+
+      keymaps = [
+        {
+          mode = [ "n" "x" "o" ];
+          key = "s";
+          action.__raw = ''function() require("flash").jump() end'';
+          options = { desc = "Flash"; };
+        }
+        {
+          mode = [ "n" "o" "x" ];
+          key = "S";
+          action.__raw = ''function() require("flash").treesitter() end'';
+          options = { desc = "Flash Treesitter"; };
+        }
+        {
+          mode = [ "o" ];
+          key = "r";
+          action.__raw = ''function() require("flash").remote() end'';
+          options = { desc = "Flash remote"; };
+        }
+        {
+          mode = [ "o" "x" ];
+          key = "R";
+          action.__raw = ''function() require("flash").treesitter_search() end'';
+          options = { desc = "Treesitter Search"; };
+        }
+      ];
+    };
+  };
+}
