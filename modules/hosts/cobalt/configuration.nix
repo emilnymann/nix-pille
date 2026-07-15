8869b391535320597f32805f81d7376e509cd6c5
Emil Nymann Sølyst <ens@uniify.io>
2026-07-15 08:24:33 +0200

    feat(cobalt): disable slack auto updates

--- modules/hosts/cobalt/configuration.nix
+++ modules/hosts/cobalt/configuration.nix
@@ -65,6 +65,22 @@
         };
       };
 
+      system.activationScripts.postActivation.text = ''
+        install -d -m 755 "/Library/Managed Preferences"
+        install -m 644 ${pkgs.writeText "com.tinyspeck.slackmacgap.plist" ''
+          <?xml version="1.0" encoding="UTF-8"?>
+          <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+          <plist version="1.0">
+            <dict>
+              <key>AutoUpdate</key>
+              <false/>
+            </dict>
+          </plist>
+        ''} "/Library/Managed Preferences/com.tinyspeck.slackmacgap.plist"
+        chown root:wheel "/Library/Managed Preferences/com.tinyspeck.slackmacgap.plist"
+        /usr/bin/killall cfprefsd || true
+      '';
+
       environment.shells = [pkgs.fish];
 
       documentation.doc.enable = false;
