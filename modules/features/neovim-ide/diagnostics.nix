d863bde890afcbdea5d0f7e665265c59a52c71f1
Emil Nymann Sølyst <ens@uniify.io>
2026-07-16 15:40:50 +0200

    feat(neovim-ide/diagnostics): set which-key group

--- modules/features/neovim-ide/diagnostics.nix
+++ modules/features/neovim-ide/diagnostics.nix
@@ -1,20 +1,32 @@
 _: {
   flake.homeModules.neovim-ide = _: {
     programs.nixvim = {
-      plugins.trouble = {
-        enable = true;
+      plugins = {
+        trouble = {
+          enable = true;
+        };
+
+        which-key = {
+          enable = true;
+          settings.spec = [
+            {
+              __unkeyed-1 = "<leader>x";
+              group = "Diagnostics";
+            }
+          ];
+        };
       };
 
       keymaps = [
         {
           key = "<leader>xx";
           action = "<cmd>Trouble diagnostics toggle filter.buf=0<cr>";
-          options = { desc = "Buffer Diagnostics (Trouble)"; };
+          options = { desc = "Buffer diagnostics"; };
         }
         {
           key = "<leader>xX";
           action = "<cmd>Trouble diagnostics toggle<cr>";
-          options = { desc = "Workspace Diagnostics (Trouble)"; };
+          options = { desc = "Workspace diagnostics"; };
         }
       ];
     };
