51dff8ff84428781f81d7001bc01601049627142
Emil Nymann Sølyst <ens@uniify.io>
2026-07-16 14:50:18 +0200

    fix(nixvim/lazygit): edit file opens buffer in existing tabpage

--- modules/features/neovim-ide/nixvim/plugins/snacks-lazygit.nix
+++ modules/features/neovim-ide/nixvim/plugins/snacks-lazygit.nix
@@ -1,38 +1,43 @@
 _: {
-  flake.homeModules.neovim-ide =
-    { pkgs, ... }:
-    {
-      programs.nixvim = {
-        extraPackages = with pkgs; [
-          neovim-remote
-        ];
+  flake.homeModules.neovim-ide = {pkgs, ...}: {
+    programs.nixvim = {
+      extraPackages = with pkgs; [
+        neovim-remote
+      ];
 
-        plugins.which-key.settings.spec = [
-          {
-            __unkeyed-1 = "<leader>g";
-            group = "Git";
-          }
-        ];
+      plugins.which-key.settings.spec = [
+        {
+          __unkeyed-1 = "<leader>g";
+          group = "Git";
+        }
+      ];
 
-        plugins.snacks = {
-          enable = true;
-          settings = {
-            lazygit = {
-              enabled = true;
+      plugins.snacks = {
+        enable = true;
+        settings = {
+          lazygit = {
+            enabled = true;
+            config = {
+              os = {
+                edit = ''sh -c 'if [ -z "$NVIM" ]; then nvim -- "$1"; else nvr --remote-send q && nvr --remote "$1"; fi' sh {{filename}}'';
+                editAtLine = ''sh -c 'if [ -z "$NVIM" ]; then nvim +"$1" -- "$2"; else nvr --remote-send q && nvr --remote +"$1" "$2"; fi' sh {{line}} {{filename}}'';
+                openDirInEditor = ''sh -c 'if [ -z "$NVIM" ]; then nvim -- "$1"; else nvr --remote-send q && nvr --remote "$1"; fi' sh {{dir}}'';
+              };
             };
           };
         };
-
-        keymaps = [
-          {
-            mode = [ "n" ];
-            key = "<leader>gg";
-            action.__raw = "function() Snacks.lazygit() end";
-            options = {
-              desc = "Git";
-            };
-          }
-        ];
       };
+
+      keymaps = [
+        {
+          mode = ["n"];
+          key = "<leader>gg";
+          action.__raw = "function() Snacks.lazygit() end";
+          options = {
+            desc = "Git";
+          };
+        }
+      ];
     };
+  };
 }
