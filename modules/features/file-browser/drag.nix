0acdf00c79778427d618806b9263afe4debf56f7
Emil Nymann Sølyst <ens@uniify.io>
2026-08-24 12:35:30 +0200

    feat(file-browser/drag): remove hyprland guard for ripdrag yazi plugin

--- modules/features/file-browser/drag.nix
+++ modules/features/file-browser/drag.nix
@@ -1,28 +1,21 @@
 _: {
-  flake.homeModules.file-browser =
-    {
-      config,
-      lib,
-      pkgs,
-      ...
-    }:
-    lib.mkIf config.wayland.windowManager.hyprland.enable {
-      programs.yazi = {
-        extraPackages = with pkgs; [
-          ripdrag
-        ];
+  flake.homeModules.file-browser = {pkgs, ...}: {
+    programs.yazi = {
+      extraPackages = with pkgs; [
+        ripdrag
+      ];
 
-        plugins = {
-          drag = pkgs.yaziPlugins.drag;
-        };
-
-        keymap.mgr.prepend_keymap = [
-          {
-            on = [ "<C-d>" ];
-            run = "plugin drag";
-            desc = "Drag Files";
-          }
-        ];
+      plugins = {
+        drag = pkgs.yaziPlugins.drag;
       };
+
+      keymap.mgr.prepend_keymap = [
+        {
+          on = ["<C-d>"];
+          run = "plugin drag";
+          desc = "Drag Files";
+        }
+      ];
     };
+  };
 }
