e6ed6c0982347e12f9c089557fa776da3e865be5
Emil Nymann Sølyst <ens@uniify.io>
2026-08-26 18:13:01 +0200

    fix(password-manager/onepassword): stop force reinstalling and corrupting database

--- modules/features/password-manager/onepassword.nix
+++ modules/features/password-manager/onepassword.nix
@@ -1,7 +1,8 @@
 _: {
   flake.homeModules.onepassword = _: {
     features.web-browser.glide.extensionLines = [
-      "glide.addons.install('https://addons.mozilla.org/firefox/downloads/file/4853670/1password_x_password_manager-8.12.32.33.xpi', { force: true, private_browsing_allowed: true });"
+      # Reinstalling on every config load can interrupt 1Password's extension database.
+      "glide.addons.install('https://addons.mozilla.org/firefox/downloads/file/4853670/1password_x_password_manager-8.12.32.33.xpi', { private_browsing_allowed: true });"
     ];
   };
 }
