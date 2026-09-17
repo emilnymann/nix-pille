57256fa5011ec2cde516b94fcd13df77fb45f7cd
Emil Nymann Sølyst <ens@uniify.io>
2026-09-17 09:16:25 +0200

    feat(neovim-ide/completion): enable friendly-snippets

--- modules/features/neovim-ide/completion.nix
+++ modules/features/neovim-ide/completion.nix
@@ -1,27 +1,24 @@
 _: {
   flake.homeModules.neovim-ide = _: {
     programs.nixvim = {
-      plugins.blink-cmp = {
-        enable = true;
-        settings = {
-          keymap = {
-            preset = "enter";
-          };
-          # Use Neovim's native vim.snippet implementation. This expands both
-          # LSP-provided snippets and the local snippets source below without
-          # pulling in friendly-snippets (or another snippet engine).
-          snippets = {
-            preset = "default";
-            expand.__raw = ''function(snippet) vim.snippet.expand(snippet) end'';
-          };
-          sources = {
-            default = ["lsp" "path" "snippets" "buffer"];
-            providers.snippets = {
-              # Use blink's built-in file-backed source, but do not scan
-              # friendly-snippets or any other external snippet collection.
-              opts.friendly_snippets = false;
+      plugins = {
+        blink-cmp = {
+          enable = true;
+          settings = {
+            keymap = {
+              preset = "enter";
+            };
+            snippets = {
+              preset = "default";
+              expand.__raw = ''function(snippet) vim.snippet.expand(snippet) end'';
+            };
+            sources = {
+              default = ["lsp" "path" "snippets" "buffer"];
             };
           };
+        };
+        friendly-snippets = {
+          enable = true;
         };
       };
     };
