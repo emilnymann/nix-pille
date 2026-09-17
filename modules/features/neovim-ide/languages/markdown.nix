6fea7abca1eb4a9b9d32a00a85960618a017692f
Emil Nymann Sølyst <ens@uniify.io>
2026-09-17 09:16:42 +0200

    feat(neovim-ide/markdown): use default settings

--- modules/features/neovim-ide/languages/markdown.nix
+++ modules/features/neovim-ide/languages/markdown.nix
@@ -13,17 +13,6 @@ _: {
         plugins = {
           render-markdown = {
             enable = true;
-            settings = {
-              code = {
-                sign = false;
-                width = "block";
-                right_pad = 1;
-              };
-              heading = {
-                sign = false;
-                icons.__empty = null;
-              };
-            };
           };
         };
       };
